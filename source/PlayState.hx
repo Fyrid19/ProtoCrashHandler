5cd1d98dc1b6120b1c2c66cafe6348f746a7be61
Fyrid19 <jkeonde@gmail.com>
2024-07-31 21:45:33 -0400

    ui elements with haxeui

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,9 +1,35 @@
 package;
 
+import flixel.FlxG;
 import flixel.FlxState;
+import flixel.FlxSprite;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+
+import haxe.ui.components.*;
 
 class PlayState extends FlxState {
+    var bg:FlxSprite;
+    var errorText:FlxText;
+    var scrollArea:VerticalScroll;
+
+    var textFont:String = 'vcr.ttf';
+
     override function create() {
         super.create();
+
+        errorText = new FlxText(0, 0, FlxG.width, 'Game Crash!');
+        errorText.setFormat(getFont(textFont), 64, FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+        add(errorText);
+
+        scrollArea = new VerticalScroll();
+        scrollArea.setSize(FlxG.width * 0.8, FlxG.height * 0.7);
+        scrollArea.screenCenter(X);
+        scrollArea.y += 100;
+        add(scrollArea);
+    }
+
+    inline function getFont(key:String) {
+        return 'assets/fonts/$key';
     }
 }
